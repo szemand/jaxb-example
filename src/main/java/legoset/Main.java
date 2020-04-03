0aa622f84d7c16e98ed3d7aac924b907cb0b15fe
Szemán Dávid <szeman.david@gmail.com>
2020-04-03 22:36:52 +0200

    Added legoset package

---
+++ src/main/java/legoset/Main.java
@@ -0,0 +1,34 @@
+package legoset;
+
+import jaxb.JAXBHelper;
+
+import java.net.URL;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        LegoSet legoSet = new LegoSet();
+        legoSet.setNumber("75211");
+        legoSet.setName("Imperial TIE Fighter");
+        legoSet.setTheme("Star Wars");
+        legoSet.setSubtheme("Solo");
+        legoSet.setYear(Year.of(2018));
+        legoSet.setPieces(519);
+        legoSet.setTags(Set.of("Starfighter", "Stormtrooper", "Star Wars", "Solo"));
+
+        ArrayList<Minifig> minifigs = new ArrayList<>();
+        minifigs.add(new Minifig("Imperial Mudtrooper", 2));
+        minifigs.add(new Minifig("Imperial Pilot", 1));
+        minifigs.add(new Minifig("Mimban Stormtrooper", 1));
+        legoSet.setMinifigs(minifigs);
+
+        legoSet.setWeight(new Weight(0.89, "kg"));
+        legoSet.setUrl(new URL("https://brickset.com/sets/75211-1/Imperial-TIE-Fighter"));
+
+        JAXBHelper.toXML(legoSet, System.out);
+    }
+}
---
+++ src/main/java/legoset/Minifig.java
@@ -0,0 +1,24 @@
+package legoset;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlValue;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+public class Minifig {
+
+    @XmlValue
+    private String name;
+
+    @XmlAttribute
+    private int count;
+
+    public Minifig(String name, int count){
+        this.name = name;
+        this.count = count;
+    }
+}
